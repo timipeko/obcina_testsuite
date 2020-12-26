427e500dc264d44193cb7025a714c40562786b4a
timipeko <timotej.peter.kovacic@gmail.com>
2020-12-26 16:49:18 +0200

    Added web crawler, fleshed out setup

--- functional_tests.py
+++ functional_tests.py
@@ -1,35 +1,124 @@
 import unittest
+import os
 from selenium import webdriver
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-
-class InvalidUrlTest(unittest.TestCase):
+from selenium.common.exceptions import StaleElementReferenceException, NoSuchElementException
+from urllib.parse import urlparse
+ 
+ 
+class LinkTest(unittest.TestCase):
     
     def setUp(self):
-        self.driver = webdriver.Firefox()
-
-    def test_404_on_invalid(self):
-        driver = self.driver
-        wait = WebDriverWait(driver, 10)
-        driver.get("http://test.zavrc.inpis.eu/invalid")
-        assert "Napaka" in driver.page_source, "No error thrown on invalid URL"
-
-    def test_valid_not404_on_valid(self):
-        driver = self.driver
-        driver.get("http://test.zavrc.inpis.eu/")
         try:
-            assert "Napaka" not in driver.page_source
+            # Ensure existance of urls.txt
+            if not os.path.exists("urls.txt"):
+                print(f'Please provide an "urls.txt" file in working directory, containing the URLs to all tested pages.')
+                self.fail()
+ 
+            # Read all urls 
+            urls_in = open('urls.txt', 'r')            
+            self.pages_urls = urls_in.readlines()
+            urls_in.close()
+ 
+            # Setup seen urls queue 
+            self.seen_links = []
+ 
+            # Array for storing unique URL types that denote specific page types
+            self.seen_parts = [] 
+ 
+            # Init web driver 
+            self.driver = webdriver.Firefox()        
+ 
+            # Flag for sucessful/failed unit test
+            self.any_failed = False
+ 
+            self.wait_timeout = 5
+ 
+            self.driver.implicitly_wait(self.wait_timeout)
+ 
+            self.base_url = ""
+ 
         except:
-            code = driver.find_element(By.ID, 'errorCode').text
-            line = driver.find_element(By.ID, 'errorLine').text
-            file = driver.find_element(By.ID, 'errorFile').text
-            print("Error "+code+" thrown on line "+line+" of file "+file)
-
-
-
+            print("Error in initialisation!")
+    
+    
+    def remove_duplicates(self, urls):        
+        
+        filtered = []
+ 
+        #Filtering step
+        for url in urls: 
+            if not url is None and '/' in url: 
+                ending = url.split("?")[0] if '?' in url else url
+                if self.base_url in url and not ending in self.seen_parts: 
+                    filtered.append(url)
+                    self.seen_parts.append(ending)
+                
+        return filtered
+ 
+ 
+ 
+ 
+    def process_url(self, url, depth=0, max_depth=4):       
+ 
+        if 'uploads' in url: # Is a file, must skip
+            return 
+ 
+        if urlparse(url).netloc != urlparse(self.base_url).netloc: # Is other domain site
+            return 
+        
+        if '#' in url: # Is empty link
+            return 
+ 
+        if 'stran' in url: # Is generic next page button
+            return 
+ 
+        driver = self.driver
+ 
+        driver.get(url)
+        if "ERROR" in driver.page_source: # An url failed 
+            print(f"FAILED {url}: Error {driver.find_element(By.ID, 'errorCode').text} thrown on line {driver.find_element(By.ID, 'errorLine').text} of file {driver.find_element(By.ID, 'errorFile').text}")
+            self.any_failed = True # Since an url failed, the generic test case will not be successful 
+ 
+        else: 
+            print(f"OK {url}")              
+            link_elts = driver.find_elements_by_css_selector(".content-wrap a")
+            urls = [link_el.get_attribute("href") for link_el in link_elts] # Find all links on page
+ 
+            urls = self.remove_duplicates(urls)                   
+ 
+            if depth < max_depth: # If max url depth not exceeded
+                for link in urls: 
+                    try:
+                        #print(f'Handling url {link}.....')
+                        if not link is None and link not in self.seen_links: # link on page has not been crawled yet 
+                            self.seen_links.append(link)
+                            if "inpis" in link: # Crawl only if is link on same domain..
+                                self.process_url(link, depth+1, max_depth)
+                    except StaleElementReferenceException as e1:                        
+                        print(e1.msg)
+                    except NoSuchElementException as e2: 
+                        print(e2.msg)
+ 
+    def test_link_crawler(self):                
+        # Get all webpage urls that will be crawled and tested 
+        for page_url in self.pages_urls:             
+            # For each web page, process all links from navbar
+            self.seen_links.append(page_url)
+            self.driver.get(page_url)
+            nav_links_xpath =  '//*[@id="wrapper"]/header//a[@href]'
+            nav_links_elts = self.driver.find_elements_by_xpath(nav_links_xpath)
+            nav_links_urls = [nav_link_elt.get_attribute("href") for nav_link_elt in nav_links_elts]
+            for nav_links_url in nav_links_urls:                
+                if not nav_links_url in self.seen_links:
+                    self.base_url = nav_links_url
+                    self.process_url(nav_links_url)
+                    self.seen_links.append(nav_links_url)
+ 
+        assert not self.any_failed, "URLs with errors exist!"
+ 
     def tearDown(self):
         self.driver.close()
-
+ 
 if __name__ == "__main__":
     unittest.main()
